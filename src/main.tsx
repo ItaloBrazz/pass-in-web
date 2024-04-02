23fe05a9b44c98bc69552bda8a4b2bb302a1bea5
ItaloBrazz <italo.brazz@outlook.com>
2024-04-01 23:08:58 -0300

    Iniciando o projeto, criando tabela e página de participantes

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {App} from './app'
+import './index.css'
+
+//Componentes e propriedades
+
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
